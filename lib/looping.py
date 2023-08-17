ede58679756805f8f152fc3d16d5007fd74fc061
mary446 <135317707+mary446@users.noreply.github.com>
2023-08-17 21:29:02 +0300

    looped through...

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,38 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+   i = 10
+   while i > 0:
+     print(i)
+     i -= 1
+   if (i == 0):
+    print("Happy New Year!")
+
+
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    squared_list = [num ** 2 for num in int_list]
+    return squared_list
+
+result = square_integers([1, 2, 3, 4, 5])
+print(result) 
+    
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    
+    count = 1
+    while count <= 100:
+        if (count % 3 == 0 and count % 5 == 0):
+            print("FizzBuzz")
+            count += 1
+        elif (count % 3 == 0):
+            print("Fizz")
+            count += 1
+        elif (count % 5 == 0):
+            print("Buzz")
+            count += 1
+        else:
+            print(count)
+            count += 1
+
